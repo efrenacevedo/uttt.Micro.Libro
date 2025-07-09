56d07c3b481ada3b7468a9f48149f952e6a57115
Efren <joseefrenacevedo3@gmail.com>
2025-07-09 09:13:06 -0600

    cambios program

--- Program.cs
+++ Program.cs
@@ -23,10 +23,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
     {
         policy
-            .WithOrigins(
-                "http://localhost:5173",
-                "https://vista-libros-autores-xge2-efrenacevedos-projects.vercel.app"
-            )
+            .AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
